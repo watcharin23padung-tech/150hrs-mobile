f164e786de48d9792e06af16ea8ff749999f2849
iFon <watcharin23.padung@gmail.com>
2026-08-27 23:27:05 +0700

    clear repo

--- supabase/client.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { createBrowserClient } from "@supabase/ssr";
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config";
-
-export function createClient() {
-  return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-}
